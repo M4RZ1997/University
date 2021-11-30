41232e19aa7a96a0f69f1e06e097e7dfa5c93c7f
Marcel <44601347+M4RZ1997@users.noreply.github.com>
2021-11-30 09:04:47 +0100

    Last finishing touches to CV A02P01

--- 2021 Herbstsemester/Computervision/Assignments/Assignment 02/Part 01/template/utils.py
+++ 2021 Herbstsemester/Computervision/Assignments/Assignment 02/Part 01/template/utils.py
@@ -30,6 +30,8 @@ def get_normalization_matrix(x):
     centroid = np.mean(x, 1)
 
     s = np.sqrt(2) / np.mean(np.apply_along_axis(lambda v: np.linalg.norm(v - centroid), 0, x))
+
+    # another approach to calculate s as found in the paper https://cs.adelaide.edu.au/~wojtek/papers/pami-nals2.pdf
     # s = 1 / np.sqrt(np.sum(np.square(np.apply_along_axis(lambda v: v - centroid, 0, x))) / (2 * x.shape[1]))
 
     T[0, 0] = s
@@ -37,14 +39,6 @@ def get_normalization_matrix(x):
     T[1, 1] = s
     T[1, 2] = - s * centroid[1]
     T[2, 2] = 1
-
-    """ Test for correctness of squared mean distance equals 2
-    norm_x = np.apply_along_axis(lambda v: np.matmul(T, v), 0, x)
-    mean = np.zeros((344, 1))
-    for i in range(norm_x.shape[1]):
-        mean[i] = np.sum(np.square(norm_x[:2, i]))
-    print(np.mean(mean))
-    """
 
     return T
 
@@ -71,22 +65,22 @@ def eight_points_algorithm(x1, x2, normalize=True):
 
         # Normalize inputs
         # TODO
-        norm_x1 = np.apply_along_axis(lambda x: np.matmul(Tx1, x), 0, x1)
-        norm_x2 = np.apply_along_axis(lambda x: np.matmul(Tx2, x), 0, x2)
+        x1 = np.apply_along_axis(lambda x: np.matmul(Tx1, x), 0, x1)
+        x2 = np.apply_along_axis(lambda x: np.matmul(Tx2, x), 0, x2)
 
     # Construct matrix A encoding the constraints on x1 and x2
     # TODO
 
     A = np.zeros((x1.shape[1], 9))
     for i in range(x1.shape[1]):
-        A[i, 0] = norm_x2[0, i] * norm_x1[0, i]
-        A[i, 1] = norm_x2[0, i] * norm_x1[1, i]
-        A[i, 2] = norm_x2[0, i]
-        A[i, 3] = norm_x2[1, i] * norm_x1[0, i]
-        A[i, 4] = norm_x2[1, i] * norm_x1[1, i]
-        A[i, 5] = norm_x2[1, i]
-        A[i, 6] = norm_x1[0, i]
-        A[i, 7] = norm_x1[1, i]
+        A[i, 0] = x2[0, i] * x1[0, i]
+        A[i, 1] = x2[0, i] * x1[1, i]
+        A[i, 2] = x2[0, i]
+        A[i, 3] = x2[1, i] * x1[0, i]
+        A[i, 4] = x2[1, i] * x1[1, i]
+        A[i, 5] = x2[1, i]
+        A[i, 6] = x1[0, i]
+        A[i, 7] = x1[1, i]
         A[i, 8] = 1.0
 
     # Solve for F using SVD
@@ -117,7 +111,7 @@ def right_epipole(F):
     Computes the (right) epipole from a fundamental matrix F.
     (Use with F.T for left epipole.)
 
-    Description EPIPOLES:
+    TODO Description EPIPOLES:
     The epipole shows where the other camera would be located on the image.
     Every epipolar line associated with a point from that camera will go through this point.
     If both images were taken parallel to each other the epipoles would be at infinity and therefore the
@@ -125,6 +119,7 @@ def right_epipole(F):
     """
 
     # The epipole is the null space of F (F * e = 0)
+    # one could also use scipy.linalg.null_space
     # e = null_space(F)
 
     U, S, V = np.linalg.svd(F)
@@ -135,14 +130,14 @@ def right_epipole(F):
     return e
 
 
-def plot_epipolar_line(im, F, x, e, plot = None):
+def plot_epipolar_line(im, F, x, e, plot=None):
     """
     Plot the epipole and epipolar line F*x=0 in an image. F is the fundamental matrix
     and x a point in the other image.
 
-    Description EPIPOLAR LINES
+    TODO Description EPIPOLAR LINES:
     The epipolar line of a point x from another image, is a line which corresponds to all possible positions at which
-    this point can be in this image. It will eventually meet all other epipolar lines at the epipole.
+    this point can be in this image. It will eventually meet all other epipolar lines of that image at the epipole.
     """
     m, n = im.shape[:2]
     #
@@ -150,7 +145,9 @@ def plot_epipolar_line(im, F, x, e, plot = None):
 
     epipolar_line = np.dot(F, x)
 
-    """
+    SAMPLE_SIZE = 4
+
+    """ Legacy Code
     sample_size = 4
     # Take sample x coordinates to create a line on the image
     image_x_coord = np.linspace(0, n, sample_size)
@@ -163,7 +160,7 @@ def plot_epipolar_line(im, F, x, e, plot = None):
     """
 
     # Refactored algorithm from above (nicer/less code)
-    image_x_coord = np.linspace(0, n, 4)
+    image_x_coord = np.linspace(0, n, SAMPLE_SIZE)
     image_y_coord = np.array([(epipolar_line[2] + epipolar_line[0] * x) / (-epipolar_line[1]) for x in image_x_coord])
 
     # Points that are outside the image are discarded
