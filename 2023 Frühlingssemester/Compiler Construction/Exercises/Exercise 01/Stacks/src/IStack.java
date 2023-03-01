d93544f5935012acae9642b04d7905cb090f4879
M4RZ1997 <m4rz@gmx.net>
2023-03-01 18:33:26 +0100

    Made Compiler Construction a submodule

--- 2023 Frühlingssemester/Compiler Construction/Exercises/Exercise 01/Stacks/src/IStack.java
+++
@@ -1,7 +0,0 @@
-public interface IStack<E> {
-        public boolean isEmpty ( ) ;
-        public int size ( ) ;
-        public void push (E item ) ;
-        public E top ( ) ;
-        public E pop ( ) ;
-}
